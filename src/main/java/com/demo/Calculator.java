84a194b5d728d7294b5bea88ce50fd406fadf125
Swarina Jaiswal <swarina.jaiswal@ukg.com>
2023-07-20 14:16:54 +0530

    Common annotations implemented

---
+++ src/main/java/com/demo/Calculator.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Calculator {
+
+    public int add (int a, int b) {
+        return a + b;
+    }
+
+    public int multiply (int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
